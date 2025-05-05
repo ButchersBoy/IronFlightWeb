495f77c5c2367c8bde5225e9d5e9b77e56e95f47
James Willock <jameswillockuk@gmail.com>
2025-05-05 08:25:02 +0100

    super basic website

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          DEFAULT: '#ffffff',
+          dark: '#000000',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
